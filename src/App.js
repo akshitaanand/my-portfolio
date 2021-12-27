097e0ee0fbbd681ad521bdc3be177ec415f774ce
akshitaanand <akshitaanand@users.noreply.github.com>
2021-12-26 20:33:18 -1000

    header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {Navbar, Nav, NavDropdown, Container} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto" >
+              <Nav.Link href="#home" style = {{marginRight: "40px"}}>About Me</Nav.Link>
+              <Nav.Link href="#link" style = {{marginRight: "40px"}}>Learning Blogs</Nav.Link>
+              <Nav.Link href="#link" style = {{marginRight: "40px"}}>Resume</Nav.Link>
+              <Nav.Link href="#link" style = {{marginRight: "40px"}}>Projects</Nav.Link>
+            </Nav>       
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+{/* <Nav className="justify-content-end" style = {{backgroundColor: "pink", height: "60px"}} activeKey="/home">
+    <Nav.Item>
+      <Nav.Link href="/home">Active</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link eventKey="link-1">Link</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link eventKey="link-2">Link</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link eventKey="disabled" disabled>
+        Disabled
+      </Nav.Link>
+    </Nav.Item>
+  </Nav> */}
+
+
     </div>
   );
 }
